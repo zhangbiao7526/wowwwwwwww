bbae87b20bd7066f196aa588860f2ae2e9402656
zhangbiao7526 <594781652@qq.com>
2014-12-18 09:59:14 +0800

    跌日次

--- src/com/example/wowwwwwwwwwwwwwwww/VARIABLE.java
+++ src/com/example/wowwwwwwwwwwwwwwww/VARIABLE.java
@@ -5,4 +5,5 @@ public class VARIABLE {
 	//public static final String LOGIN="http://10.30.6.85:8080/togoProjectDemo/servlet/user_Login";
 	public static final String SERVER="10.30.6.85";
 		int aaaa;
+		String s="caacaca";
 	}
